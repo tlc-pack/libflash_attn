2c80621cc49b94ce455113769de7690421d34f94
zhiwei <zfang@octo.ai>
2024-09-10 21:27:48 +0000

    update cutlass and fa3 condition

--- src/flash.cu
+++ src/flash.cu
@@ -31,7 +31,7 @@ void run(Flash_fwd_params params, cudaStream_t stream) {
   cudaDeviceGetAttribute(&minor, cudaDevAttrComputeCapabilityMinor, device);
   params.sm = major * 10 + minor;
 
-  if(params.sm >= 90 && (params.d == 64 || params.d == 128 || params.d == 256)) {
+  if(params.sm >= 90 && (params.d == 64 || params.d == 128 || params.d == 256) && (params.window_size_left < 0 && params.window_size_right < 0) && !params.is_causal) {
     if (params.is_e4m3) {
       if (params.d == 64) {
         flash3::run_mha_fwd_<cutlass::float_e4m3_t, 64>(params, stream);
